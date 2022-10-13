f287f3be7fbdb944c7fe80d282881e2ebee73132
Abhyudai Singh <axs7941@mavs.uta.edu>
2022-10-13 03:12:23 -0500

    homepage commit

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Team
 
 # Register your models here.
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'designation']
+    pass
+admin.site.register(Team, TeamAdmin)
